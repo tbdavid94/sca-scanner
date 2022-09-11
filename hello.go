b7a36d18b2dc452d08d77a98636efb1cf36bd124
david.tunglt <david.tunglt@gmail.com>
2022-09-11 21:54:40 +0700

    test handle report

--- hello.go
+++ hello.go
@@ -2,10 +2,36 @@ package main
 
 import (
 	"fmt"
-
-	"github.com/google/go-cmp/cmp"
+	"github.com/PuerkitoBio/goquery"
+	"log"
+	"os"
+	"strings"
 )
 
+func GenerateIndexFile() {
+	resultReport, err := os.ReadFile("reports/index.html") // just pass the file name
+	if err != nil {
+		fmt.Print(err)
+	}
+
+	fmt.Println(resultReport) // print the content as 'bytes'
+	resultReportStr := string(resultReport) // convert content to a 'string'
+	fmt.Println(resultReportStr) // print the content as a 'string'
+
+	strReader := strings.NewReader(resultReportStr)
+
+	// Load the HTML document
+	doc, err := goquery.NewDocumentFromReader(strReader)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Find element need remove
+	doc.Find("#test").Remove()
+	//elementString := string(element)
+	//fmt.Print(string(element))
+}
+
 func main() {
-	fmt.Println(cmp.Diff("Hello World", "Hello Go"))
+	GenerateIndexFile()
 }
