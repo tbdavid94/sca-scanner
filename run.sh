6af0abc5d60d6f4b2985d4b8db1dfb91ac9f44e9
david.tunglt <david.tunglt@gmail.com>
2022-09-13 05:57:44 +0700

    change fossa runsh the 1st

--- run.sh
+++ run.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
 #depscan --report_file reports/depscan.json --sync
-echo "==== DEPSCAN & SCAN ===="
-python3 /usr/local/src/scan
 echo "==== FOSSA CLI ===="
 fossa analyze -o > reports/logs-output.json
+echo "==== DEPSCAN & SCAN ===="
+python3 /usr/local/src/scan
 echo "DONE"
